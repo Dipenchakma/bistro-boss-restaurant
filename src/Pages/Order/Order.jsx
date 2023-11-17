896ccd807d991350e8874acaa294ea51f6259efe
dipen <dipen@dipens-MacBook-Air.local>
2023-11-17 15:57:05 +0600

    order menu

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -4,14 +4,17 @@ import Cover from "../../Shared/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { useState } from "react";
+import FoodCard from "../../Components/FoodCard/FoodCard";
 
 const Order = () => {
   const [tabIndex, setTabIndex] = useState(0);
   const [menu] = UseMenu();
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const soups = menu.filter((item) => item.category === "soup");
   const desserts = menu.filter((item) => item.category === "dessert");
-  const soup = menu.filter((item) => item.category === "soup");
-
-
+  const drinks = menu.filter((item) => item.category === "drinks");
+  console.log("drinks", drinks);
   return (
     <div>
       <Cover img={orderImg} title="Our shop"></Cover>
@@ -27,12 +30,43 @@ const Order = () => {
           <Tab>Desserts</Tab>
           <Tab>Drinks</Tab>
         </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3  justify-center">
+            {salads.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          {" "}
+          <div className="grid md:grid-cols-3 justify-center">
+            {pizzas.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3  justify-center">
+            {soups.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 justify-center">
+            {desserts.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 justify-center items-center">
+            {drinks.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+      
       </Tabs>
     </div>
   );
