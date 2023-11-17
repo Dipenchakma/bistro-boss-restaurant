729f168e92b3d297900db2892593a34cd8e3b6f8
dipen <dipen@dipens-MacBook-Air.local>
2023-11-17 15:31:35 +0600

    food menu card

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,14 +1,41 @@
 import UseMenu from "../../Shared/Custom hook/UseMenu";
-import orderImg from '../../../bistro-boss-restaurant-resources-main/assets/menu/banner3.jpg'
+import orderImg from "../../../bistro-boss-restaurant-resources-main/assets/menu/banner3.jpg";
 import Cover from "../../Shared/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
+
 const Order = () => {
-    const [menu] = UseMenu()
-    console.log('menu', menu)
-    return (
-        <div>
-           <Cover img ={orderImg} title= 'Our shop'></Cover>
-        </div>
-    );
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = UseMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+
+
+  return (
+    <div>
+      <Cover img={orderImg} title="Our shop"></Cover>
+      <Tabs
+        className="text-center my-10"
+        defaultIndex={tabIndex}
+        onSelect={(index) => setTabIndex(index)}
+      >
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
